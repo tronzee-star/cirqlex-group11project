a22ff827bbf49daae6635d2391d279bc3c39c98f
Gloria Birundu <birundugloria@gmail.com>
2025-10-23 02:09:21 +0300

    added layout file

---
+++ client/src/components/shared/Layout.jsx
@@ -0,0 +1,12 @@
+import Navbar from './Navbar';
+import Footer from './footer';
+
+export default function Layout({ children }) {
+  return (
+    <div className="min-h-screen flex flex-col bg-white text-black font-sans">
+      <Navbar />
+      <main className="flex-grow">{children}</main>
+      <Footer />
+    </div>
+  );
+}
