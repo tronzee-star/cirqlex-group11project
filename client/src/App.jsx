bf915c8dacc1650f864820a6f9fadbd35190ecf6
Bonnie <bonfasogaro@gmail.com>
2025-10-24 01:05:15 +0300

    refactor: simplify App component structure by removing Layout and unused imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,24 @@
-import Layout from './components/shared/Layout';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from "./components/shared/Navbar";
+import Footer from "./components/shared/Footer";
 import Home from './pages/Home';
 import BuyerDashboard from './pages/buyerDashboard';
 import Shop from './pages/shop';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <Layout>
+      <Navbar />
+      {/* add top padding so fixed navbar doesn't cover content */}
+      <div className="pt-20">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/buyer-dashboard" element={<BuyerDashboard />} />
           <Route path="/shop" element={<Shop />} />
         </Routes>
-      </Layout>
-    </Router>
-/*import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';*/
-import Navbar from "./components/shared/Navbar";
-import Hero from "./components/shared/Hero";
-import Features from "./components/shared/Features";
-import ImpactSection from "./components/shared/ImpactSection";
-import Footer from "./components/shared/Footer";
-
-/*import Home from './pages/Home';*/
-/*import SignUp from './pages/auth/SignUp';*/
-/*import SignIn from './pages/auth/SignIn';*/
-
-function App() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <Features />
-      <ImpactSection />
+      </div>
       <Footer />
-    </>
+    </Router>
   );
 }
 
