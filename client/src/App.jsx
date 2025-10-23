920875505da7a54009f69b000f98c996d007618f
Gloria Birundu <birundugloria@gmail.com>
2025-10-23 23:50:48 +0300

    feat: refactor App component to use a new layout with Navbar, Hero, Features, ImpactSection, and Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,25 @@
-import Layout from './components/shared/Layout';
-import Home from './pages/Home';
+/*import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';*/
+import Navbar from "./components/shared/Navbar";
+import Hero from "./components/shared/Hero";
+import Features from "./components/shared/Features";
+import ImpactSection from "./components/shared/ImpactSection";
+import Footer from "./components/shared/Footer";
+
+/*import Home from './pages/Home';*/
+/*import SignUp from './pages/auth/SignUp';*/
+/*import SignIn from './pages/auth/SignIn';*/
 
 function App() {
   return (
-    <Layout>
-      <Home />
-    </Layout>
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <ImpactSection />
+      <Footer />
+    </>
   );
 }
 
 export default App;
+
