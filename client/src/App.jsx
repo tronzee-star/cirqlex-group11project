b8dde1cf30f5d1e201830be6a12656e4af45ec89
Bonnie <bonfasogaro@gmail.com>
2025-10-23 21:51:24 +0300

    refactor: enhance routing structure in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 import Layout from './components/shared/Layout';
 import Home from './pages/Home';
+import BuyerDashboard from './pages/buyerDashboard';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Layout>
-      <Home />
-    </Layout>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/buyer-dashboard" element={<BuyerDashboard />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
